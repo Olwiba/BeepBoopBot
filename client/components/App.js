b90d895df8e4a11d61c0f6b3f900424c8062ddf3
olwiba <olwiba@gmail.com>
2016-07-29 10:28:10 +1200

    Added navigation and resources dir

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import Nav from './Nav';
 
 class App extends Component {
   static propTypes = {
@@ -14,7 +15,9 @@ class App extends Component {
 
   render() {
     return (
-      <div>App</div>
+      <div>
+        <Nav />
+      </div>
     );
   }
 }
