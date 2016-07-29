b885bce4d9d36f5bf8f51dbbcc91d0ec9b3629b4
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-07-30 10:10:13 +1200

    Remove react motion stuff to see if travis passes

--- client/components/Board.js
+++ client/components/Board.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Motion, spring } from 'react-motion'
+// import { Motion, spring } from 'react-motion'
 
 class Board extends Component {
 
@@ -10,19 +10,6 @@ class Board extends Component {
         <p>
           THIS IS THE BOARD
         </p>
-        <Motion defaultStyle={{x: 0}} style={{x: spring(200)}}>
-          {value => {
-            let style = {
-              width: 100,
-              height: 100,
-              position: 'absolute',
-              top: 200,
-              left: value.x,
-              border: '1px solid red'
-            }
-            return (<div style={style}>{value.x}</div>)
-          }}
-        </Motion>
       </div>
     )
   }
