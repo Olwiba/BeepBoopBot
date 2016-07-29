16e4a7d42245c03e4d77dee011bc9340bbd57a87
Troy Lamerton <troy.lamerton@gmail.com>
2016-07-29 17:22:11 +1200

    Add robot tests and robot constructor

---
+++ client/components/models/Robot.js
@@ -0,0 +1,15 @@
+export default class Robot {
+
+  constructor () {
+    this.direction = 0
+    this.isOnBox = false
+    this.positionX = 0
+    this.positionY = 0
+  }
+
+  moveForward () {}
+
+  left () {}
+
+  right () {}
+}
--- client/test/robot.js
+++ client/test/robot.js
@@ -1,8 +1,39 @@
 import Robot from '../robot.js'
 import test from 'tape'
 
-test('Robot object', (t) => {
+test('Creating a new robot', (t) => {
   const robot = new Robot()
 
   t.ok(robot, 'Robot created is truthy')
+
+  t.equal(robot.positionX, 0, 'Robot starts at X position of 0')
+  t.equal(robot.positionY, 0, 'Robot starts at Y position of 0')
+
+  t.false(robot.isOnBox, 'Robot is not on a box')
+
+  t.equal(robot.direction, 0, 'Robot starts looking straight ahead')
+})
+
+test('Robot movement', (t) => {
+  let robot = new Robot()
+  const startY = robot.positionY
+  const startX = robot.positionX
+
+  robot.moveForward()
+
+  t.equal(robot.positionY, startY, 'Robot can move forward')
+
+  robot = new Robot()
+  robot.right()
+
+  t.true(robot.direction === 90, 'Robot can turn right')
+
+  robot = new Robot()
+  robot.left()
+
+  t.true(robot.direction === -90 || robot.direction === 270, 'Robot can turn left')
+
+  robot.moveForward()
+
+  t.equal(robot.positionX, startX + 1, 'Robot moves in correct direction after turning left')
 })
