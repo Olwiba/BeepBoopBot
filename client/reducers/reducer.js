3689902f026ffa0614184c16f77bdb3a709cd23a
Troy Lamerton <troy.lamerton@gmail.com>
2016-07-30 15:04:22 +1200

    Add movement actions to reducer

--- client/reducers/reducer.js
+++ client/reducers/reducer.js
@@ -15,6 +15,9 @@ const INITIAL_STATE = {
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 1]
+  ],
+  commandQueue: [
+
   ]
 }
 
@@ -26,38 +29,74 @@ const reducer = (state = INITIAL_STATE, action) => {
     case 'STOP_BUTTON':
       return state
 
-    case 'ADD_FORWARD':
+    case 'MOVE_FORWARD':
       const newFwdState = {
         robot: {...state.robot},
-        board: {...state.board}
+        board: [...state.board],
+        commandQueue: [...state.commandQueue]
       }
       moveForward(newFwdState.robot, newFwdState.board)
       return newFwdState
 
-    case 'ADD_LEFT':
+    case 'TURN_LEFT':
       const newLeftState = {
         robot: {...state.robot},
-        board: {...state.board}
+        board: [...state.board],
+        commandQueue: [...state.commandQueue]
       }
       // If direction is 0, set it to 270, otherwise subtract 90
       newLeftState.robot.direction = newLeftState.robot.direction ? newLeftState.robot.direction - 90 : 270
       return newLeftState
 
-    case 'ADD_RIGHT':
+    case 'TURN_RIGHT':
       const newRightState = {
         robot: {...state.robot},
-        board: {...state.board}
+        board: [...state.board],
+        commandQueue: [...state.commandQueue]
       }
       // If direction is 270, set it to 0, otherwise add 90
       newRightState.robot.direction = newRightState.robot.direction === 270 ? 0 : newRightState.robot.direction + 90
       return newRightState
 
+    case 'JUMP_UP':
+      const jumpState = {
+        robot: {...state.robot},
+        board: [...state.board],
+        commandQueue: [...state.commandQueue]
+      }
+      jump(jumpState.robot, jumpState.board)
+      return jumpState
+
+    case 'ADD_FORWARD':
+      const newFwdState = {
+        robot: {...state.robot},
+        board: [...state.board],
+        commandQueue: [...state.commandQueue]
+      }
+      return newFwdState
+
+    case 'ADD_LEFT':
+      const newLeftState = {
+        robot: {...state.robot},
+        board: [...state.board],
+        commandQueue: [...state.commandQueue]
+      }
+      return newLeftState
+
+    case 'ADD_RIGHT':
+      const newRightState = {
+        robot: {...state.robot},
+        board: [...state.board],
+        commandQueue: [...state.commandQueue]
+      }
+      return newRightState
+
     case 'ADD_JUMP':
       const jumpState = {
         robot: {...state.robot},
-        board: {...state.board}
+        board: [...state.board],
+        commandQueue: [...state.commandQueue]
       }
-      jump(jumpState.robot, jumpState.board)
       return jumpState
 
     default:
