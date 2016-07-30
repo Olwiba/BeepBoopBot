c76bac455bb711786c2924db380fecbab3155f5c
Charlotte Smith <hello@seanjohnson.nz>
2016-07-30 13:56:24 +1200

    created board

--- client/components/Board.js
+++ client/components/Board.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react'
-import { Motion, spring } from 'react-motion'
+import { Motion, spring, StaggeredMotion, TransitionMotion } from 'react-motion'
 
 class Board extends Component {
+
+  constructor(props) {
+      super(props)
+
+      const levelOne = [
+                      [0,0,0,2,1],
+                      [0,2,0,2,0],
+                      [0,0,0,2,1],
+                      [0,2,0,2,0],
+                      [0,0,0,2,1]
+                    ]
+
+      const idNum = [0,0,2,0,1,0,0,2,0,1,0,0,2,0,1,0,0,2,0,1]
+      this.state = { levelOne };
+
+  }
+
+
 
   render () {
     console.log(this.props)
     return (
       <div id='board'>
-        <p>
-          THIS IS THE BOARD
-        </p>
-        <Motion defaultStyle={{x: 0}} style={{x: spring(200)}}>
-          {value => {
-            let style = {
-              width: 100,
-              height: 100,
-              position: 'absolute',
-              top: 200,
-              left: value.x,
-              border: '1px solid red'
-            }
-            return (<div style={style}>{value.x}</div>)
-          }}
-        </Motion>
+        <div className="board-container">
+          { this.state.levelOne.map((row) => {
+            return row.map((id, index) => (
+              <Motion defaultStyle={{size: 1, }} style={{size: spring(18, {stiffness: 180, damping: 12})}}>
+                {value => <div key={index} className="tile" style={{
+                  height: value.size + '%',
+                  width: value.size + '%'
+                }}>{id}</div>}
+              </Motion>
+            ))
+          })}
+        </div>
       </div>
     )
   }
@@ -30,3 +44,11 @@ class Board extends Component {
 }
 
 export default Board
+
+
+// <Motion defaultStyle={{size: 1, }} style={{size: spring(18, {stiffness: 180, damping: 12})}}>
+//   {value => <div className="tile" style={{
+//     height: value.size + '%',
+//     width: value.size + '%'
+//   }}>{value.x}</div>}
+// </Motion>
