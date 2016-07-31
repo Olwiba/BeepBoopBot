bd7d2b19a347881d16bc01d398b8efc66c894623
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-07-31 14:30:54 +1200

    Robot movement working

--- client/components/Robot.js
+++ client/components/Robot.js
@@ -3,29 +3,21 @@ import { Motion, spring, StaggeredMotion, TransitionMotion } from 'react-motion'
 
 class Robot extends Component {
 
-  constructor (props) {
-    super(props)
-    // Find tile at robot's current position
+  calcCenter () {
     var y = this.props.robot.positionY.toString()
     var x = this.props.robot.positionX.toString()
     var bounds = this.props.tileInfo[y + x].getBoundingClientRect()
-    console.log('bounds', bounds)
     var centerX = (bounds.right + bounds.left) / 2
     var centerY = (bounds.top + bounds.bottom) / 2
-    console.log('XX', centerX, centerY)
-    this.state = {
-      centerX: centerX,
-      centerY: centerY
-    }
+    return [centerX, centerY]
   }
 
   render () {
-    console.log('robot props', this.props)
-    console.log('robot state', this.state)
+    var centerPoints = this.calcCenter()
     return (
-      <Motion defaultStyle={{x: 0, y: 0}} style={{x: spring(this.state.centerX), y: spring(this.state.centerY)}}>
+      <Motion defaultStyle={{x: 0, y: 0, rot: 0}} style={{x: spring(centerPoints[0]), y: spring(centerPoints[1]), rot: spring(this.props.robot.direction)}}>
         {value => <div
-          style={{height: 30, width: 30, border: 'solid 1px blue', position: 'absolute', top: value.y, left: value.x, transform: `rotate(${0}deg)`}}
+          style={{height: 30, width: 30, border: 'solid 1px blue', position: 'absolute', top: value.y - 15, left: value.x - 15, transform: `rotate(${value.rot}deg)`}}
       >HI2</div>}
       </Motion>
     )
