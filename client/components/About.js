f6452cf8a10ce901440560486941d6fb59dad96c
Apprentice <apprentice@StMac0-6.local>
2016-07-31 17:36:15 +1200

    add about component and make the modal

---
+++ client/components/About.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import SkyLight from 'react-skylight';
+
+class About extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      // state goes here
+    }
+  }
+  componentDidMount() {
+    this.refs.aboutBox.show()
+  }
+
+  render () {
+    const style = {
+      backgroundColor: '#00897B',
+      color: '#ffffff',
+      width: '70%',
+      height: '600px',
+      marginTop: '-300px',
+      marginLeft: '-35%',
+    };
+
+    return (
+      <div>
+        <button onClick={() => this.refs.aboutBox.show()}>Open Modal</button>
+        <SkyLight dialogStyles={style} hideOnOverlayClicked ref="aboutBox" >
+          I"'"m a custom modal!
+        </SkyLight>
+      </div>
+    )
+  }
+}
+
+export default About
