ca37085d8d54b2cbfccf99c2b28f9ef24731ca72
olwiba <olwiba@gmail.com>
2016-08-01 21:55:36 +1200

    Added pit styling and changed level 10 to have some pits

--- client/components/Board.js
+++ client/components/Board.js
@@ -26,9 +26,12 @@ class Board extends Component {
                 let oddEven = counter % 2 == 1 ? 'odd' : 'even'
                 counter++
                 return (
+                col == 3 ?
+                <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile clear'} ref={rowIndex.toString() + colIndex.toString()}></div> :
                 col == 2 ? 
                 <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <img src='/resources/images/box-tile.svg' className='box-tile' /> </div>  :
-                 col == 1 ?  <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <div className='elevator-bottom'> <img src='/resources/images/elevator-top.svg' className='elevator-tile' /> </div> </div> :
+                 col == 1 ?
+                   <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <div className='elevator-bottom'> <img src='/resources/images/elevator-top.svg' className='elevator-tile' /> </div> </div> :
                   <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}></div>
                 )
               })
