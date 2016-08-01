817c459fa7513e8207bf40d1f2982f31c5f45bbd
Troy Lamerton <troy.lamerton@gmail.com>
2016-08-02 09:54:40 +1200

    Replace counter with logic to tell if a tile is odd or even

--- client/components/Board.js
+++ client/components/Board.js
@@ -14,25 +14,22 @@ class Board extends Component {
   }
 
   render () {
-    var counter = 0
     var levelTheme = this.props.currentLevel < 5 ? 'basement' : 'cargo'
-    console.log('the level ', levelTheme)
     return (
       <div id='board'>
         <div className='board-container'>
           {
             this.props.board.map((row, rowIndex) => {
               return row.map((col, colIndex) => {
-                let oddEven = counter % 2 == 1 ? 'odd' : 'even'
-                counter++
+                let oddEven = (rowIndex + colIndex) % 2 === 1 ? 'odd' : 'even'
                 return (
-                col == 3 ?
-                <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile clear'} ref={rowIndex.toString() + colIndex.toString()}></div> :
-                col == 2 ? 
-                <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <img src='/resources/images/box-tile.svg' className='box-tile' /> </div>  :
-                 col == 1 ?
-                   <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <div className='elevator-bottom'> <img src='/resources/images/elevator-top.svg' className='elevator-tile' /> </div> </div> :
-                  <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}></div>
+                col === 3 ?
+                <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile clear'} ref={rowIndex.toString() + colIndex.toString()}></div>
+                : col === 2 ?
+                  <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <img src='/resources/images/box-tile.svg' className='box-tile' /> </div>
+                  : col === 1 ?
+                    <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <div className='elevator-bottom'> <img src='/resources/images/elevator-top.svg' className='elevator-tile' /> </div> </div>
+                    : <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}></div>
                 )
               })
             })
