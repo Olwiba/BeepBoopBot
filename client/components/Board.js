445a89fce8c3d7218ec7a22ac5100ff6a6ca2e40
Troy Lamerton <troy.lamerton@gmail.com>
2016-08-02 10:08:09 +1200

    Use classnames module for concatenating class names

--- client/components/Board.js
+++ client/components/Board.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import RobotContainer from '../containers/RobotContainer'
+import classNames from 'classnames'
 
 class Board extends Component {
 
@@ -23,13 +24,13 @@ class Board extends Component {
               return row.map((col, colIndex) => {
                 let oddEven = (rowIndex + colIndex) % 2 === 1 ? 'odd' : 'even'
                 return (
-                col === 3 ?
-                <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile clear'} ref={rowIndex.toString() + colIndex.toString()}></div>
-                : col === 2 ?
-                  <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <img src='/resources/images/box-tile.svg' className='box-tile' /> </div>
-                  : col === 1 ?
-                    <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <div className='elevator-bottom'> <img src='/resources/images/elevator-top.svg' className='elevator-tile' /> </div> </div>
-                    : <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}></div>
+                  col === 3 ?
+                    <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={classNames('tile', 'clear')} ref={rowIndex.toString() + colIndex.toString()}></div>
+                    : col === 2 ?
+                      <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={classNames('tile', levelTheme, oddEven)} ref={rowIndex.toString() + colIndex.toString()}> <img src='/resources/images/box-tile.svg' className='box-tile' /> </div>
+                      : col === 1 ?
+                        <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={classNames('tile', levelTheme, oddEven)} ref={rowIndex.toString() + colIndex.toString()}> <div className='elevator-bottom'> <img src='/resources/images/elevator-top.svg' className='elevator-tile' /> </div> </div>
+                        : <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={classNames('tile', levelTheme, oddEven)} ref={rowIndex.toString() + colIndex.toString()}></div>
                 )
               })
             })
