78a04c1e7d83e81f5a124b6afc7c7d1da71400a1
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-08-01 19:42:11 +1200

    Backstory, howToPlay and Win modals all working as desired

--- client/components/Win.js
+++ client/components/Win.js
@@ -1,24 +1,20 @@
-import {Component} from 'react'
+import React, {Component} from 'react'
 import SkyLight from 'react-skylight'
 
 class Win extends Component {
-  constructor(props){
-    super(props);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-  }
 
-  handleKeyPress(e) {
-    if(e.which === 27) {
+  handleKeyPress (e) {
+    if (e.which === 27) {
       this.refs.winBox.hide()
     }
   }
 
   componentDidMount () {
-    document.addEventListener('keyup', this.handleKeyPress)
+    document.addEventListener('keyup', this.handleKeyPress.bind(this))
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('keyup', this.handleKeyPress);
+  componentWillUnmount () {
+    document.removeEventListener('keyup', this.handleKeyPress.bind(this))
   }
 
   componentDidUpdate () {
@@ -31,23 +27,12 @@ class Win extends Component {
     this.props.SELECT_LEVEL(newLevel)
   }
 
-  render() {
+  render () {
     console.log('win props', this.props)
-    const style = {
-      backgroundColor: '#00897B',
-      color: '#ffffff',
-      width: '70%',
-      height: '600px',
-      marginTop: '-300px',
-      marginLeft: '-35%'
-    }
-    const divStyle = {
-      width: '100%',
-      height: '100%'
-    }
     return (
-      <SkyLight dialogStyles={style} beforeClose={this._executeBeforeModalClose.bind(this)} hideOnOverlayClicked={true} ref="winBox" >
-        <div style={divStyle}onClick={()=>{this.refs.winBox.hide()}}> Click Me to Close </div>
+      <SkyLight beforeClose={this._executeBeforeModalClose.bind(this)} hideOnOverlayClicked={true} ref='winBox' >
+        <p>You win!!</p>
+        <button onClick={() => { this.refs.winBox.hide() }}>OK</button>
       </SkyLight>
     )
   }
