9e05d892214f09823b56596ae105948ab985cec0
olwiba <olwiba@gmail.com>
2016-08-01 21:05:07 +1200

    Got level themes working and styled tiles

--- client/components/Board.js
+++ client/components/Board.js
@@ -14,17 +14,22 @@ class Board extends Component {
   }
 
   render () {
+    var counter = 0
+    var levelTheme = this.props.currentLevel < 5 ? 'basement' : 'cargo'
+    console.log('the level ', levelTheme)
     return (
       <div id='board'>
         <div className='board-container'>
           {
             this.props.board.map((row, rowIndex) => {
               return row.map((col, colIndex) => {
+                let oddEven = counter % 2 == 1 ? 'odd' : 'even'
+                counter++
                 return (
                 col == 2 ? 
-                <div key={parseInt(rowIndex.toString() + colIndex.toString())} className='tile' ref={rowIndex.toString() + colIndex.toString()}> <img src='/resources/images/box-tile.svg' className='box-tile' /> </div>  :
-                 col == 1 ?  <div key={parseInt(rowIndex.toString() + colIndex.toString())} className='tile' ref={rowIndex.toString() + colIndex.toString()}> <div className='elevator-bottom'> <img src='/resources/images/elevator-top.svg' className='elevator-tile' /> </div> </div> :
-                  <div key={parseInt(rowIndex.toString() + colIndex.toString())} className='tile' ref={rowIndex.toString() + colIndex.toString()}></div>
+                <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <img src='/resources/images/box-tile.svg' className='box-tile' /> </div>  :
+                 col == 1 ?  <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}> <div className='elevator-bottom'> <img src='/resources/images/elevator-top.svg' className='elevator-tile' /> </div> </div> :
+                  <div key={parseInt(rowIndex.toString() + colIndex.toString())} className={'tile ' + levelTheme + ' ' + oddEven} ref={rowIndex.toString() + colIndex.toString()}></div>
                 )
               })
             })
