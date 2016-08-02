c43db48eedc92b6e5ba720d8407d822a42af393e
Vaishnavi Iyer <email": null>
2016-08-02 13:29:37 +1200

    removed dispatch from robot container

--- client/containers/RobotContainer.js
+++ client/containers/RobotContainer.js
@@ -1,16 +1,8 @@
 import { connect } from 'react-redux'
 import Robot from '../components/Robot'
-import * as actions from '../reducers/action.js'
 
 function mapStateToProps (state) {
   return state
 }
-function mapDispatchToProps (dispatch) {
-  return {
-    hasFinished: () => {
-      dispatch(actions.hasFinished)
-    }
-  }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Robot)
+export default connect(mapStateToProps)(Robot)
