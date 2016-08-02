3506339963e3072d813c1b86ae76aa3b486c7a29
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-08-02 19:28:50 +1200

    replace level select dropdown with basic component

---
+++ client/components/LevelSelect.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import SkyLight from 'react-skylight'
+
+class LevelSelect extends Component {
+
+  render () {
+    return (
+      <div className='levels'>SELECT A LEVELLLL</div>
+    )
+  }
+
+}
+
+export default LevelSelect
