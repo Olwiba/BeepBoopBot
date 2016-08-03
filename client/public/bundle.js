b4b9f55420172b48efa458a5333f6fc74361b12c
Troy Lamerton <troy.lamerton@gmail.com>
2016-08-03 16:53:58 +1200

    Add uglifyify to webpack config

---
+++ client/public/bundle.js
@@ -0,0 +1,1 @@
+!function(n){function r(t){if(o[t])return o[t].exports;var e=o[t]={exports:{},id:t,loaded:!1};return n[t].call(e.exports,e,e.exports,r),e.loaded=!0,e.exports}var o={};return r.m=n,r.c=o,r.p="/",r(0)}([function(n,r,o){!function(){throw new Error('Cannot find module "./client/index.js"')}()}]);
